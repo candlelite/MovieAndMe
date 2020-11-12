b3e569ef7632894ddbbac10574d00572e876d111
candlelite <tirel@noos.fr>
2020-11-12 12:21:35 +0100

    Intégrez Redux dans votre application

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux'
+import Store from './Store/configureStore'
 import Search from './Components/Search'
 import FilmDetail from './Components/FilmDetail'
 
@@ -10,18 +12,22 @@ const SearchStackNavigator = createStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <SearchStackNavigator.Navigator initialRouteName="Search">
-        <SearchStackNavigator.Screen
-          name="Search"
-          component={Search}
-          options={{ title: 'Rechercher' }} />
-        <SearchStackNavigator.Screen
-          name="FilmDetail"
-          component={FilmDetail}
-          options={{ title: 'Détail du film' }} />
-      </SearchStackNavigator.Navigator>
-    </NavigationContainer>
+    <Provider store={Store}>
+      <NavigationContainer>
+        <SearchStackNavigator.Navigator initialRouteName="Search">
+          <SearchStackNavigator.Screen
+            name="Search"
+            component={Search}
+            options={{ title: 'Rechercher' }}
+          />
+          <SearchStackNavigator.Screen
+            name="FilmDetail"
+            component={FilmDetail}
+            options={{ title: 'Détail du film' }}
+          />
+        </SearchStackNavigator.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
