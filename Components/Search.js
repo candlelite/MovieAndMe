991478109682f365f0255aea07560e53a8d74f15
candlelite <cat_bear75@yahoo.fr>
2020-11-17 17:42:16 +0100

    Externaliser la FlatList de l'affichage de la liste de films.

--- Components/Search.js
+++ Components/Search.js
@@ -3,6 +3,7 @@
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi' // import { } from ... car c'est un export nommé dans TMDBApi.js
 //import films from '../Helpers/filmsData'
 import FilmItem from './FilmItem'
+import FilmList from './FilmList'
 import FilmDetail from './FilmDetail'
 import React from 'react'
 import { StyleSheet, View, TextInput, Button, FlatList, Text, ActivityIndicator } from 'react-native'
@@ -61,14 +62,6 @@ class Search extends React.Component {
       })
   }
 
-  _displayDetailForFilm = (idFilm) => {
-      console.log("Display film with id " + idFilm)
-      this.props.navigation.navigate('FilmDetail', {
-            idFilm: idFilm,
-            otherParam: 'anything you want here',
-      });
-  }
-
   render() {
     return (
       <View style={styles.main_container}>
@@ -80,24 +73,12 @@ class Search extends React.Component {
         />
         <Button title='Rechercher' onPress={() => this._searchFilms()}/>
         {/* Ici j'ai simplement repris l'exemple sur la documentation de la FlatList */}
-        <FlatList
-          data={this.state.films}
-          extraData={this.props.favoritesFilm}
-          // On utilise la prop extraData pour indiquer à notre FlatList que d’autres données doivent être prises en compte si on lui demande de se re-rendre
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({item}) =>
-            <FilmItem
-              film={item}
-              isFilmFavorite={(this.props.favoritesFilm.findIndex(film => film.id === item.id) !== -1) ? true : false}
-              displayDetailForFilm={this._displayDetailForFilm}
-            />
-          }
-          onEndReachedThreshold={0.5}
-          onEndReached={() => {
-            if (this.page < this.totalPages) { // On vérifie qu'on n'a pas atteint la fin de la pagination (totalPages) avant de charger plus d'éléments
-              this._loadFilms()
-            }
-          }}
+        <FilmList
+          films={this.state.films} // C'est bien le component Search qui récupère les films depuis l'API et on les transmet ici pour que le component FilmList les affiche
+          navigation={this.props.navigation} // Ici on transmet les informations de navigation pour permettre au component FilmList de naviguer vers le détail d'un film
+          loadFilms={this._loadFilms} // _loadFilm charge les films suivants, ça concerne l'API, le component FilmList va juste appeler cette méthode quand l'utilisateur aura parcouru tous les films et c'est le component Search qui lui fournira les films suivants
+          page={this.page}
+          totalPages={this.totalPages} // les infos page et totalPages vont être utile, côté component FilmList, pour ne pas déclencher l'évènement pour charger plus de film si on a atteint la dernière page
         />
         {this._displayLoading()}
       </View>
