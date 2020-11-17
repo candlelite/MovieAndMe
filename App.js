2f479e55a4da659dfe66d16339f58245164a9d24
candlelite <tirel@noos.fr>
2020-11-17 13:35:20 +0100

    Ajout d'un TabNavigator, qui contien un/des StackNavigator

--- App.js
+++ App.js
@@ -3,29 +3,51 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Provider } from 'react-redux'
 import Store from './Store/configureStore'
 import Search from './Components/Search'
+import Favorites from './Components/Favorites'
 import FilmDetail from './Components/FilmDetail'
 
 const SearchStackNavigator = createStackNavigator();
+const MoviesTabNavigator = createBottomTabNavigator();
+
+function SearchStackScreen() {
+  return (
+    <SearchStackNavigator.Navigator initialRouteName="Search">
+      <SearchStackNavigator.Screen
+        name="Search"
+        component={Search}
+        options={{ title: 'Rechercher' }}
+      />
+      <SearchStackNavigator.Screen
+        name="FilmDetail"
+        component={FilmDetail}
+        options={{ title: 'Détail du film' }}
+      />
+    </SearchStackNavigator.Navigator>
+  );
+}
 
 function App() {
   return (
     <Provider store={Store}>
       <NavigationContainer>
-        <SearchStackNavigator.Navigator initialRouteName="Search">
-          <SearchStackNavigator.Screen
+
+        <MoviesTabNavigator.Navigator>
+          <MoviesTabNavigator.Screen
             name="Search"
-            component={Search}
+            component={SearchStackScreen}
             options={{ title: 'Rechercher' }}
           />
-          <SearchStackNavigator.Screen
-            name="FilmDetail"
-            component={FilmDetail}
-            options={{ title: 'Détail du film' }}
+          <MoviesTabNavigator.Screen
+            name="Favorites"
+            component={Favorites}
+            options={{ title: 'Favoris' }}
           />
-        </SearchStackNavigator.Navigator>
+        </MoviesTabNavigator.Navigator>
+
       </NavigationContainer>
     </Provider>
   );
